3b199bb9992c88239f55d4f781dcd09fffa3f49d
SalaheddineFikri <salaheddine.fikri1@gmail.com>
2025-06-24 19:26:39 -0400

    v-0.5

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["images.unsplash.com","assets.aceternity.com", "upload.wikimedia.org"],
+    domains: [
+      "images.unsplash.com",
+      "assets.aceternity.com",
+      "upload.wikimedia.org",
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true, 
   },
 };
 
