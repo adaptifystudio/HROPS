4da9a189978f431193e0b3b3489dcf7102c985c0
SalaheddineFikri <salaheddine.fikri1@gmail.com>
2025-06-24 23:24:08 -0400

    v-2.3

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -3,20 +3,12 @@ import BlogModel from "@/app/models/Blog";
 import { notFound } from "next/navigation";
 import { BlogContentWithToc } from "./BlogContentWithToc";
 
-export async function generateStaticParams() {
-  return []; 
-}
+export default async function BlogPage(props: any) {
+  const { slug } = props.params;
 
-interface PageProps {
-  params: {
-    slug: string;
-  };
-}
-
-export default async function BlogPage({ params }: PageProps) {
   await dbConnect();
+  const blog = await BlogModel.findById(slug).lean();
 
-  const blog = await BlogModel.findById(params.slug).lean();
   if (!blog) return notFound();
 
   return <BlogContentWithToc blog={JSON.parse(JSON.stringify(blog))} />;
